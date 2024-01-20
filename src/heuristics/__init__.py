7f78dcbcd38ef737a32a68404479d86501334985
Paul Brenker <paul.brenker@smail.inf.h-brs.de>
2024-01-20 22:18:43 +0100

    make heuristics.py readable

--- src/heuristics/__init__.py
+++ src/heuristics/__init__.py
@@ -7,11 +7,8 @@
 from .heuristics import christo
 from .heuristics import mst
 from .heuristics import onetree
-#from .heuristics import assrel  ## FORBIDDEN
-#from .heuristics import ci  ## FORBIDDEN
 from .heuristics import fi
 from .heuristics import greedy
-from .heuristics import lplb
 from .heuristics import mstheu
 from .heuristics import ni
 from .heuristics import nn
